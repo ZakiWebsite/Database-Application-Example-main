10486fc9b9e8954299d584f334dd951ffd9fbe18
ZakiWebsite <24038@burnside.school.nz>
2026-04-21 14:38:53 +1200

    lesson 2
    cleaned up output

--- Database-Application-Example-main/app.py
+++ Database-Application-Example-main/app.py
@@ -5,14 +5,21 @@ import sqlite3
 # variables
 DATABASE = 'skyscrapers.db'
 
+
 # functions
-
+def print_all_buildings():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM building_list;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print out the heading
+    print('Name                           Height(m)  City               Year Built')
+    # loop through all the results
+    for tower in results:
+        print(f"{tower[1]:<30} {tower[2]:<10} {tower[3]:<18} {tower[4]}")
+    # loop finish
+    db.close()
 # main code
-
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * FROM building_list;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+print_all_buildings()
